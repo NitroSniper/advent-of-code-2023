1c6f751f0d01ac9ff2fa3ef19bd28e9473d4128d
NitroSniper <mahiemiah35@gmail.com>
2023-12-16 16:34:34 +0000

    Bug Fixes: Wrong index to access correct arguments

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,8 +3,8 @@ use std::env;
 
 fn main() {
     let input = include_str!("../data/input");
-    match env::args()
-        .nth(2)
+    let result = match env::args()
+        .nth(1)
         .expect("Please add an argument of either '1' OR '2' for part 1 / 2 respectively ")
         .as_str()
     {
@@ -12,4 +12,5 @@ fn main() {
         "2" => library::part2::problem(input),
         _ => panic!("This is not a valid argument"),
     };
+    dbg!(result);
 }

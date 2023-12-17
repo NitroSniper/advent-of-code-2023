9ad2e05a246cbc8c2c1f9d2b6744c616f597816b
NitroSniper <mahiemiah35@gmail.com>
2023-12-17 16:49:05 +0000

    Completed Part 1 and 2. Need to do rewrite

--- src/part1.rs
+++ src/part1.rs
@@ -1,5 +1,21 @@
 pub fn problem(input: &str) -> String {
-    todo!()
+    input
+        .split_terminator('\n')
+        .map(|line| {
+            10 * line
+                .chars()
+                .nth(line.find(char::is_numeric).expect("a number"))
+                .expect("THERE IS A NUMBER")
+                .to_digit(10)
+                .expect("it will happen")
+                + line
+                    .chars()
+                    .nth(line.rfind(char::is_numeric).expect("a number"))
+                    .expect("THERE IS A NUMBER")
+                    .to_digit(10)
+                    .expect("it will happen")
+        })
+        .sum::<u32>().to_string()
 }
 
 #[cfg(test)]
@@ -9,6 +25,6 @@ mod test {
     #[test]
     fn it_works() {
         let result = problem(include_str!("./data/test_1"));
-        assert_eq!(result, "TODO!")
+        assert_eq!(result, "142")
     }
 }
--- src/part2.rs
+++ src/part2.rs
@@ -1,5 +1,72 @@
+#[derive(Debug)]
+struct NumberLocation {
+    location: usize,
+    number: u32,
+}
+
 pub fn problem(input: &str) -> String {
-    todo!();
+    let numbers = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let mut total = 0;
+    for line in input.split_terminator('\n') {
+        let mut r_loc: Option<NumberLocation> = None;
+        if let Some(index) = line.rfind(|c: char| c.is_ascii_digit()) {
+            let r = line.chars().nth(index).unwrap().to_digit(10).unwrap();
+            r_loc = Some(NumberLocation {
+                location: index,
+                number: r,
+            });
+        }
+        let mut l_loc: Option<NumberLocation> = None;
+        if let Some(index) = line.find(|c: char| c.is_ascii_digit()) {
+            let l = line.chars().nth(index).unwrap().to_digit(10).unwrap();
+            l_loc = Some(NumberLocation {
+                location: index,
+                number: l,
+            });
+        }
+        dbg!(line);
+        for (number, number_as_str) in numbers.into_iter().enumerate() {
+            if let Some(index) = line.rfind(number_as_str) {
+                /*
+                 * If r_loc is Some AND > than index then keep it. otherwise remove it.
+                 * if r_loc is None OR r_loc < index then replace it
+                 */
+
+                if !r_loc
+                    .as_ref()
+                    .is_some_and(|num_loc| num_loc.location > index)
+                {
+                    r_loc = Some(NumberLocation {
+                        location: index,
+                        number: number as u32,
+                    })
+                }
+            }
+
+            if let Some(index) = line.find(number_as_str) {
+                /*
+                 * If l_loc is Some AND > than index then keep it. otherwise remove it.
+                 * if l_loc is None OR r_loc < index then replace it
+                 */
+
+                if !l_loc
+                    .as_ref()
+                    .is_some_and(|num_loc| num_loc.location < index)
+                {
+                    l_loc = Some(NumberLocation {
+                        location: index,
+                        number: number as u32,
+                    })
+                }
+            }
+        }
+        println!("{:?}", l_loc);
+        println!("{:?}", r_loc);
+        total += l_loc.expect("it").number*10 + r_loc.expect("it").number
+    }
+    total.to_string()
 }
 
 #[cfg(test)]
@@ -9,6 +76,6 @@ mod test {
     #[test]
     fn it_works() {
         let result = problem(include_str!("./data/test_2"));
-        assert_eq!(result, "TODO!")
+        assert_eq!(result, "281")
     }
 }
